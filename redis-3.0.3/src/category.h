b2a539241235bd95c9b784cc57f825a0e6108795
z3635363 <245767493@qq.com>
2016-06-06 16:02:59 +0800

    delete copy

--- redis-3.0.3/src/category.h
+++
@@ -1,9 +0,0 @@
-#include "redis.h"
-#include "rio.h"
-
-long long categoryObjectSize(robj *o);
-void doCalculateCategory();
-void ccCommand(redisClient *c);
-void killccCommand(redisClient *c);
-void addCateforyStats(robj *key, long long size, dict* d);
-
